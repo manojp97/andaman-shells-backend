d72d11e8d988c10a04eadcacfe6e9c8bddfbc3d7
manojp97 <manojprajapati97563684@gmail.com>
2026-06-15 12:02:53 +0530

    backend is ready

--- src/app.js
+++ src/app.js
@@ -16,7 +16,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://andaman-shells-umpm.vercel.app/"
+    ],
     credentials: true,
   })
 );
