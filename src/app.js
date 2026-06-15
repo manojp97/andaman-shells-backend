65c0b946c83dda36d6cb1da8a7dd02d718226320
manojp97 <manojprajapati97563684@gmail.com>
2026-06-15 12:08:20 +0530

    Fix Vercel CORS

--- src/app.js
+++ src/app.js
@@ -14,12 +14,20 @@ app.use(requestLogger);
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://andaman-shells-umpm.vercel.app",
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://andaman-shells-umpm.vercel.app/"
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
